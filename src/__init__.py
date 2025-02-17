95791d26feb37a55fd624972aa6999006bfd82d6
Pranav Sharma <pranvshrma13@gmail.com>
2025-02-17 11:45:51 +0530

    setup.py and requirements

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# Its primary purpose is to mark a directory as a Python package. 
+# When Python encounters a directory containing __init__.py, 
+# it treats that directory as a package rather than just a regular folder.
